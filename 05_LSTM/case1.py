1899c498e1b91f5f8f20bb7f53b772f744b2056d
ccKiven <h1819987310@gmail.com>
2024-01-17 19:51:05 +0800

    update LSTM

--- 05_LSTM/case1.py
+++ 05_LSTM/case1.py
@@ -1,4 +1,4 @@
-from Data import np, pd, Data
+from Data import np, pd, StandardScaler, Data
 from time import time
 from datetime import date
 import tensorflow as tf
@@ -29,8 +29,8 @@ def main():
     print(f"Processing data is done, cost {time()-s_clk:.3f} s")
 
     # 2 - 模拟预测
-    # result_path = "./../result/05case1.xlsx"
-    # writer = pd.ExcelWriter(result_path)
+    result_path = "./../result/05case1.xlsx"
+    writer = pd.ExcelWriter(result_path)
     # 设置模型
     model = Sequential()
     model.add(LSTM(units=10, activation='relu', input_shape=(
@@ -41,10 +41,11 @@ def main():
     model.compile(optimizer='adam', loss='mse', metrics=['mape'])
 
     for step in sliding_step:
-        iter_num = month_difference//step
+        iter_num = month_difference//step  # 迭代次数
         train_list = [None]*iter_num
         test_list = [None]*iter_num
         err_arr = np.zeros(shape=(iter_num, fd_arr.shape[0]))
+
         for i in range(fd_arr.shape[0]):
             fd = fd_arr[i]  # 预测未来第几天
             # 初始化参数
@@ -69,49 +70,46 @@ def main():
                 # 训练
                 model.fit(x_train, y_train, epochs=20, batch_size=32,
                           validation_data=(x_test, y_test))
-                return
-                # 测试
-                # 预测
-                y_pred = None
+                # 在测试集上预测
+                y_pred = StandardScaler().inverse_transform(model.predict(x_test))
                 # 计算误差
-                y_err = np.abs(y_pred/y_test-1)
-                err_arr[j, i] = np.mean(y_err)
+                err_arr[j, i] = np.abs(y_pred/y_test-1).mean()
                 # 更新训练区间
                 tw_pre += step
                 tw_end += step
-                train_range[data.ldi[tw_pre]+1, test_range[1]]
+                train_range = [data.ldi[tw_pre]+1, test_range[1]]
                 if train_list[j+1] is None:  # 保存训练区间
                     train_list[j+1] = data.scope2str(train_range)
 
             # 最后一次
+            # 划分输入和输出集
             x_train = data.get_input(train_range, fd, ud)
             y_train = data.close[train_range[0]:train_range[1]+1]
-            # 训练
-
-            # 测试
             test_range = [train_range[1]+1, data.ldi[tw_end]]
             if test_list[-1] is None:
                 test_list[-1] = data.scope2str(test_range)
-            # 预测
             x_test = data.get_input(test_range, fd, ud)
             y_test = data.close[test_range[0]:test_range[1]+1]
-            y_pred = None
+            # 训练
+            model.fit(x_train, y_train, epochs=20, batch_size=32,
+                      validation_data=(x_test, y_test))
+            # 在测试集上预测
+            y_pred = StandardScaler().inverse_transform(model.predict(x_test))
             # 计算误差
-            y_err = np.abs(y_pred/y_test-1)
-            err_arr[-1, i] = y_err.mean()
+            err_arr[-1, i] = np.abs(y_pred/y_test-1).mean()
 
             print(f"step={step}m, fd={fd}d is done, cost {time()-s_clk:.3f} s")
 
         # 保存结果
-        # sheet_name = f"step={step}m"
-        # values = np.column_stack((train_list, test_list, err_arr))
-        # cols = ['train', 'test', [f'err-{fd}d' for fd in fd_arr]]
-        # tmp_df = pd.DataFrame(values, columns=cols)
-        # tmp_df.loc[iter_num, tmp_df.columns[0]
-        #            ] = f"time cost = {time()-s_clk:.3f} s"
-        # tmp_df.to_excel(writer, sheet_name=sheet_name, index=True)
+        sheet_name = f"step={step}m"
+        values = np.column_stack((train_list, test_list, err_arr))
+        cols = ['train', 'test']+[f'err-{fd}d' for fd in fd_arr]
+        tmp_df = pd.DataFrame(values, columns=cols)
+        tmp_df.loc[iter_num, tmp_df.columns[0]
+                   ] = f"time cost = {time()-s_clk:.3f} s"
+        tmp_df.to_excel(writer, sheet_name=sheet_name, index=True)
 
-    # writer.close()
+    writer.close()
 
 
 if __name__ == "__main__":
